38091a7afbe36d458bd0861d5f3474ae46200988
Naftali Berger <bn26665@gmail.com>
2025-07-16 17:02:25 +0300

    end CRAD for riddels

--- DAL/dal.js
+++ DAL/dal.js
@@ -3,26 +3,26 @@ import { ObjectId } from "mongodb";
 
 export async function getAllriddels() {
   const db = await connectDB();
-  return db.collection("products").find().toArray();
+  return db.collection("riddels").find().toArray();
 }
 
 export async function addriddels(product) {
   const db = await connectDB();
-  return db.collection("products").insertOne(product);
+  return db.collection("riddels").insertOne(product);
 }
 
-export async function getRiddelById(id) {
-  const db = await connectDB();
-  return db.collection("products").findOne({ _id: id });
-}
+// export async function getRiddelById(id) {
+//   const db = await connectDB();
+//   return db.collection("riddels").findOne({ _id: id });
+// }
 
 export async function updateRiddelById(id, product) {
   const db = await connectDB();
-  return db.collection("products").updateOne({ _id: new ObjectId(id) },
+  return db.collection("riddels").updateOne({ _id: new ObjectId(id) },
   { $set: product });
 }
 
 export async function deleteRiddleById(id) {
   const db = await connectDB();
-  return db.collection("products").deleteOne({ _id: new ObjectId(id) });
+  return db.collection("riddels").deleteOne({ _id: new ObjectId(id) });
 }
